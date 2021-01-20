6cb8ff6590a901eddb412d76be02fab9d9c5f78d
JosiasLopes <jobercardinho2014@gmail.com>
2021-01-19 23:54:23 -0300

    Primeiro commit da aplicação

---
+++ WebApplication3/Models/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    //a classe context implementa a interface DbContext
+    //que sobreescreve o metodo OnConfiguring esse metodo é responsavel por configurar
+    //o entityFramework
+    public class Context:DbContext
+
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder opt)
+        {
+            opt.UseSqlServer(connectionString:@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=true");
+        }
+    }
+}
